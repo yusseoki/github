0f048b8a16defb16aed7e45275769b973c32e64b
yusseoki <parkey0478@naver.com>
2025-01-23 18:03:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 300 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -3,17 +3,55 @@
 #include <algorithm>
 using namespace std;
 
+vector<int>arr;
+vector<int>temp;
+
+void merge_sort(int start, int end) {
+	if (end - start < 1)
+		return;
+
+	int middle = start + (end - start) / 2;
+
+	merge_sort(start, middle);
+	merge_sort(middle + 1, end);
+
+	for (int i = start; i <= end; i++)
+		temp[i] = arr[i];
+
+	int k = start;
+	int index1 = start;
+	int index2 = middle + 1;
+
+	while (index1 <= middle && index2 <= end) {
+		if (temp[index1] < temp[index2])
+			arr[k++] = temp[index1++];
+		else
+			arr[k++] = temp[index2++];
+	}
+
+	while (index1 <= middle) {
+		arr[k++] = temp[index1++];
+	}
+	while (index2 <= end) {
+		arr[k++] = temp[index2++];
+	}
+}
+
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	int n;
 	cin >> n;
-	vector<int>arr(n, 0);
+	arr = vector<int>(n + 1, 0);
+	temp = vector<int>(n + 1, 0);
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 		cin >> arr[i];
 
-	sort(arr.begin(), arr.end());
+	merge_sort(1, n);
 
-	for (int i = 0; i < arr.size(); i++) {
+	for (int i = 1; i <= n; i++) {
 		cout << arr[i] << "\n";
 	}
 }
